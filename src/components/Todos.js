2994e6306d43d81f597fd0d70e48018acde54ac2
Moilanen Annika <s2100141@edu.bc.fi>
2021-08-18 19:42:09 +0300

    todolist

---
+++ src/components/Todos.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import {
+  addTodos,
+  completeTodos,
+  removeTodos,
+  updateTodos,
+} from "../redux/reducer";
+import { GoPlus } from "react-icons/go";
+
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+    removeTodo: (obj) => dispatch(removeTodos(obj)),
+    updateTodo: (obj) => dispatch(updateTodos(obj)),
+    completeTodo: (obj) => dispatch(completeTodos(obj)),
+  };
+};
+
+const Todos = (props) => {
+  console.log("props", props);
+
+  const [todo, setTodo] = useState("");
+
+  const add = () => {
+    if (todo === "") {
+      alert("Input is Empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
+    }
+  };
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  return (
+    <div className="addTodos">
+      <input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        className="todo-input"
+        value={todo}
+      />
+
+      <button className="add-btn" onClick={() => add()}>
+        <GoPlus />
+      </button>
+      <br />
+
+      <ul>
+        {props.todos.length > 0 &&
+          props.todos.map((item) => {
+            return <li key={item.id}>{item.item}</li>;
+          })}
+      </ul>
+    </div>
+  );
+};
+
+//we can use connect method to connect this component with redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
